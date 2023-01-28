c8886a4ba6b293af714e0a11382a3841e6331d2f
Dananjaya Ramanayake <dananjaya@cynetis-embedded.com>
2023-01-28 12:22:21 +0100

    add tests

--- logger_test.go
+++ logger_test.go
@@ -1,12 +1,59 @@
 package pocketlog_test
 
 import (
-	"os"
-	"pocketlog"
+	"testing"
+
+	"github.com/dananjayavr/pocketlog"
 )
 
+type testWriter struct {
+	contents string
+}
+
+const (
+	debugMessage = "Why write I still all one, ever the same,"
+	infoMessage  = "And keep invention in a noted weed,"
+	errorMessage = "That every word doth almost tell my name,"
+)
+
+func TestLogger_DebugfInfofErrorf(t *testing.T) {
+	type testCase struct {
+		level    pocketlog.Level
+		expected string
+	}
+
+	tt := map[string]testCase{
+		"debug": {
+			level:    pocketlog.LevelDebug,
+			expected: debugMessage + "\n" + infoMessage + "\n" + errorMessage + "\n",
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			tw := &testWriter{}
+
+			testedLogger := pocketlog.New(tc.level, pocketlog.WithOutput(tw))
+
+			testedLogger.Debugf(debugMessage)
+			testedLogger.Infof(infoMessage)
+			testedLogger.Errorf(errorMessage)
+
+			if tw.contents != tc.expected {
+				t.Errorf("invalid contents, expected: %q got %q", tc.expected, tw.contents)
+			}
+		})
+	}
+}
+
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug)
 	debugLogger.Debugf("Hello, %s", "world")
 	// Output: Hello, world
+}
+
+// Write implements the io.Writer interface.
+func (tw *testWriter) Write(p []byte) (n int, err error) {
+	tw.contents = tw.contents + string(p)
+	return len(p), nil
 }
